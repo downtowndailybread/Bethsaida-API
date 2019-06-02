081fa712d49a775d7c8cd3bfdc303701cafc254b
Andrew Guenin <andyguenin@gmail.com>
2019-06-02 12:05:56 -0400

    remove test code

--- src/main/scala/org/downtowndailybread/bethsaida/providers/DatabaseConnectionProvider.scala
+++ src/main/scala/org/downtowndailybread/bethsaida/providers/DatabaseConnectionProvider.scala
@@ -5,20 +5,6 @@ import java.sql.Connection
 trait DatabaseConnectionProvider {
   this: SettingsProvider =>
 
-
-//  def runSql[T](funct: (Connection) => T): T = {
-//    val connection = settings.ds.getConnection()
-//    try {
-//      val result = funct(connection)
-//      result
-//    } catch {
-//      case e: Exception =>
-//        throw e
-//    } finally {
-//      connection.close()
-//    }
-//  }
-
   def runSql[T](funct: (Connection) => T): T = {
     val connection = settings.ds.getConnection()
     connection.setAutoCommit(false)
